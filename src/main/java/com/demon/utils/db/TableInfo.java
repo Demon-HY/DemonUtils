740caa58941e67395e4234bc04754b54418f4697
heyan <heyan@xubei.com>
2018-03-12 14:54:00 +0800

    添加数据库反射操作类 CommonDao，以及MapUtils

---
+++ src/main/java/com/demon/utils/db/TableInfo.java
@@ -0,0 +1,42 @@
+package com.demon.utils.db;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 数据库表信息
+ *
+ * Created by yhe.abcft on 2017/7/9 0009.
+ */
+public class TableInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String tableName;
+    private String tableComment;
+    private List<TableFieldInfo> listFieldInfos;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public String getTableComment() {
+        return tableComment;
+    }
+
+    public List<TableFieldInfo> getListFieldInfos() {
+        return listFieldInfos;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public void setTableComment(String tableComment) {
+        this.tableComment = tableComment;
+    }
+
+    public void setListFieldInfos(List<TableFieldInfo> listFieldInfos) {
+        this.listFieldInfos = listFieldInfos;
+    }
+}
